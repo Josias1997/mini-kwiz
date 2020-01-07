e0255d9c4b558c120025720a08009a45290d8536
Josias <kologojosias@gmail.com>
2020-01-07 11:08:11 +0100

    changed port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var kwiz = require('./kwiz_module/kwiz_module');
 
 //create the server and the socketsio
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
 
 //server static file in the public directory
 app.use(express.static('public'))
@@ -24,9 +24,10 @@ app.use(express.static('public'))
 // client connexion
 io.on('connection', function (socket) {
     //send the questions to the client
+    console.log("client connected");
     socket.emit("quiz", kwiz.questions());
 });
 
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 8080, () => {
 	console.log("Server is running");
 });
